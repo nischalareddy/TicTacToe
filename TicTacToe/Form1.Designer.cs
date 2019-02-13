98c4699c1a2c2b768eb4f2a208e9217699a3ccd9
nischalareddy <43692326+nischalareddy@users.noreply.github.com>
2019-02-13 11:13:02 -0800

    TicTacToe Project

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.tb4 = new System.Windows.Forms.TextBox();
+            this.tb5 = new System.Windows.Forms.TextBox();
+            this.tb6 = new System.Windows.Forms.TextBox();
+            this.tb7 = new System.Windows.Forms.TextBox();
+            this.tb8 = new System.Windows.Forms.TextBox();
+            this.tb9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb1.Location = new System.Drawing.Point(118, 120);
+            this.tb1.MaxLength = 1;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(41, 32);
+            this.tb1.TabIndex = 0;
+            this.tb1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb1.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb2
+            // 
+            this.tb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb2.Location = new System.Drawing.Point(209, 120);
+            this.tb2.MaxLength = 1;
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(41, 32);
+            this.tb2.TabIndex = 1;
+            this.tb2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb2.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb3
+            // 
+            this.tb3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb3.Location = new System.Drawing.Point(296, 120);
+            this.tb3.MaxLength = 1;
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(41, 32);
+            this.tb3.TabIndex = 2;
+            this.tb3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb3.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb4
+            // 
+            this.tb4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb4.Location = new System.Drawing.Point(118, 196);
+            this.tb4.MaxLength = 1;
+            this.tb4.Name = "tb4";
+            this.tb4.Size = new System.Drawing.Size(41, 32);
+            this.tb4.TabIndex = 3;
+            this.tb4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb4.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb5
+            // 
+            this.tb5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb5.Location = new System.Drawing.Point(209, 196);
+            this.tb5.MaxLength = 1;
+            this.tb5.Name = "tb5";
+            this.tb5.Size = new System.Drawing.Size(41, 32);
+            this.tb5.TabIndex = 4;
+            this.tb5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb5.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb6
+            // 
+            this.tb6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb6.Location = new System.Drawing.Point(296, 196);
+            this.tb6.MaxLength = 1;
+            this.tb6.Name = "tb6";
+            this.tb6.Size = new System.Drawing.Size(41, 32);
+            this.tb6.TabIndex = 5;
+            this.tb6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb6.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb7
+            // 
+            this.tb7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb7.Location = new System.Drawing.Point(118, 272);
+            this.tb7.MaxLength = 1;
+            this.tb7.Name = "tb7";
+            this.tb7.Size = new System.Drawing.Size(41, 32);
+            this.tb7.TabIndex = 6;
+            this.tb7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb7.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb8
+            // 
+            this.tb8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb8.Location = new System.Drawing.Point(209, 272);
+            this.tb8.MaxLength = 1;
+            this.tb8.Name = "tb8";
+            this.tb8.Size = new System.Drawing.Size(41, 32);
+            this.tb8.TabIndex = 7;
+            this.tb8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb8.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb9
+            // 
+            this.tb9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb9.Location = new System.Drawing.Point(296, 272);
+            this.tb9.MaxLength = 1;
+            this.tb9.Name = "tb9";
+            this.tb9.Size = new System.Drawing.Size(41, 32);
+            this.tb9.TabIndex = 8;
+            this.tb9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb9.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 421);
+            this.Controls.Add(this.tb9);
+            this.Controls.Add(this.tb8);
+            this.Controls.Add(this.tb7);
+            this.Controls.Add(this.tb6);
+            this.Controls.Add(this.tb5);
+            this.Controls.Add(this.tb4);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TicTacToe - By Nischala";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.TextBox tb4;
+        private System.Windows.Forms.TextBox tb5;
+        private System.Windows.Forms.TextBox tb6;
+        private System.Windows.Forms.TextBox tb7;
+        private System.Windows.Forms.TextBox tb8;
+        private System.Windows.Forms.TextBox tb9;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void InitializeGame()
+        {
+            tb1.Text = "";
+            tb2.Text = "";
+            tb3.Text = "";
+            tb4.Text = "";
+            tb5.Text = "";
+            tb6.Text = "";
+            tb7.Text = "";
+            tb8.Text = "";
+            tb9.Text = "";
+            tb1.Enabled = true;
+            tb2.Enabled = true;
+            tb3.Enabled = true;
+            tb4.Enabled = true;
+            tb5.Enabled = true;
+            tb6.Enabled = true;
+            tb7.Enabled = true;
+            tb8.Enabled = true;
+            tb9.Enabled = true;
+            tb1.Select();
+
+        }
+        public bool Match()
+        {
+            if ((tb1.Text == tb2.Text && tb1.Text == tb3.Text && tb1.Text != "") ||
+                    (tb1.Text == tb4.Text && tb1.Text == tb7.Text && tb1.Text != "") ||
+                    (tb1.Text == tb5.Text && tb1.Text == tb9.Text && tb1.Text != "") ||
+                    (tb2.Text == tb5.Text && tb2.Text == tb8.Text && tb2.Text != "") ||
+                    (tb3.Text == tb6.Text && tb3.Text == tb9.Text && tb3.Text != "") ||
+                    (tb4.Text == tb5.Text && tb4.Text == tb6.Text && tb4.Text != "") ||
+                    (tb3.Text == tb5.Text && tb3.Text == tb7.Text && tb3.Text != "") ||
+                    (tb7.Text == tb8.Text && tb7.Text == tb9.Text && tb7.Text != ""))
+                return true;
+            else
+                return false;
+        }
+
+        public bool Tie()
+        {
+            if (tb1.Text != "" &&
+                tb2.Text != "" &&
+                tb3.Text != "" &&
+                tb4.Text != "" &&
+                tb5.Text != "" &&
+                tb6.Text != "" &&
+                tb7.Text != "" &&
+                tb8.Text != "" &&
+                tb9.Text != "")
+                return true;
+            else
+                return false;
+
+        }
+        private void tb_TextChanged(object sender, EventArgs e)
+        {
+            TextBox obj = (TextBox)sender;
+            if (obj.Text == "o" || obj.Text == "x")
+            {
+                obj.Enabled = false;
+                
+                    if(Match())
+                    {
+                        MessageBox.Show("Game won by " + obj.Text);
+                        InitializeGame();
+                    }
+                    else if(Tie())
+                    {
+                        MessageBox.Show("Its a Tie Start a New Game");
+                        InitializeGame();
+                    }
+
+            }
+            else
+            {
+                obj.Text = "";
+            }
+        }
+    }
+}
